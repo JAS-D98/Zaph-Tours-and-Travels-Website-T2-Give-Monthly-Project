46c4d495d698126ed4be10ca4a475ce8a4565308
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-06-06 21:14:21 +0300

    edit font weight of navbar text

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,17 +4,17 @@ import { logo } from '../../assets/index.js'
 
 export default function Navbar() {
   return (
-    <div className='flex w-full px-5 py-1 justify-between items-center border-b-2'>
+    <div className='flex w-full px-5 py-1 justify-between items-center border-b-2 capitalize'>
       <div className='flex items-center'>
         <div className='w-[4em] overflow-hidden object-cover'>
           <img src={logo} alt="image of logo icon" width="100%" height="100%"/>
         </div>
-        <p className='text-xl font-bold'>Zaph<br/> <span className='text-green'>Tours</span></p>
+        <p className='text-xl font-semibold'>Zaph<br/> <span className='text-green'>Tours</span></p>
       </div>
       <nav className='flex gap-7 items-center'>
          {navLinks.map((link) => (
            <p key={link.label} className='text-xl font-bold-400'>
-                <Link to={link.href} className='hover:text-green'>{link.label}</Link>
+                <a href={link.href} className='hover:text-green'>{link.label}</a>
             </p>
         ))}
       </nav>
